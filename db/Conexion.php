c8ce2dde7c692225217ec8b77f1510528ebec024
oscardcd <oscardcd17@gmail.com>
2020-11-11 22:09:53 -0500

    mod cx

--- db/Conexion.php
+++ db/Conexion.php
@@ -1,15 +1,16 @@
 <?php
 class Conectar
 {
+
     public static function conexion()
     {
-        try {
-            $conexion = new mysqli("localhost", "root", "", "lavapp");
-            $conexion->query("SET NAMES 'utf8'");    
-        } catch (\Throwable $th) {
-            throw $th;
-        }
 
+        try {
+            $conexion = new PDO('mysql:host=localhost;dbname=lavapp', 'root', '');
+        } catch (\PDOException $e) {
+            //throw $th;
+            echo "error: " . $e->getMessage();
+        }
         return $conexion;
     }
 }
