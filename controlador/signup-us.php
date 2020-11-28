f5e71d5502f03906184cc705ba5a0402af8c85ab
oscardcd <oscardcd17@gmail.com>
2020-11-28 18:07:49 -0500

    subida oscar

--- controlador/signup-us.php
+++ controlador/signup-us.php
@@ -2,7 +2,11 @@
 require '../modelo/user_model.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-  $user_info=array($_POST['nombre'],$_POST['apellido'],$_POST['email'],$_POST['telefono'],$_POST['password']);
+  $user_info=array('nombre'=>$_POST['nombre'],
+                    'apellido'=>$_POST['apellido'],
+                    'email'=>$_POST['email'],
+                    'telefono'=>$_POST['telefono'],
+                    'password'=>$_POST['password']);
 }
 
 if(isset($user_info))
--- modelo/user_model.php
+++ modelo/user_model.php
@@ -2,23 +2,28 @@
  require '../db/Conexion.php';
 
  
-
+function findUser(array $user)
+{
+        $sql= "select * from usuario where email=".$user['email'];
+}
  function insert_user(array $user)
     {
         try {
             $db=conexion();
-            if(!empty($user[0]) && !empty($user[1]) && !empty($user[2]) && !empty($user[3]) && !empty($user[4]))
+            if(!empty($user['nombre']) && !empty($user['apellido']) && !empty($user['email']) && !empty($user['telefono']) && !empty($user['password']))
           {
           
               $SQL="INSERT INTO usuario (nombre,apellido,email,telefono,password) VALUES (:nombre, :apellido, :email, :telefono, :password)";
               $ps=$db->prepare($SQL);
-              $ps->bindParam(':nombre', $user[0]);
-              $ps->bindParam(':apellido', $user[1]);
-              $ps->bindParam(':email', $user[2]);
-              $ps->bindParam(':telefono', $user[3]);
-              $password = password_hash($user[4], PASSWORD_BCRYPT);
+              $ps->bindParam(':nombre', $user['nombre']);
+              $ps->bindParam(':apellido', $user['apellido']);
+              $ps->bindParam(':email', $user['email']);
+              $ps->bindParam(':telefono', $user['telefono']);
+              $password = password_hash($user['password'], PASSWORD_BCRYPT);
               $ps->bindParam(':password',$password);
           
+
+
               if ($ps->execute()) 
               {
                   return true;
