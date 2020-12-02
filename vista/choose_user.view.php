f48763a82c7554281ff93065ea61ab9182568a7e
oscardcd <oscardcd17@gmail.com>
2020-12-01 20:52:37 -0500

    subida oscar

--- vista/choose_user.view.php
+++ vista/choose_user.view.php
@@ -3,13 +3,15 @@ require 'header.php';
 ?>
 <body>
     <div class="contenedor">
-        <div class="choose_user">
-            <h1>Usuario</h1>
+        <div class="chooser">
+            <div class="choose_user">
+                <h1>Usuario</h1>
+            </div>        
+            <div class="choose_user">
+                <h1>Prestador de<br> servicio</h1>
+            </div>
         </div>
         
-        <div class="choose_user">
-            <h1>Prestador <br>de servicio</h1>
-        </div>
     </div>
 </body>
 </html>
